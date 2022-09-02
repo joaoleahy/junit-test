4f8195f119090973461866dab669e0a7efb47ddb
João Leahy <jvm.leahy@gmail.com>
2022-09-02 17:20:27 -0300

    FullWithdraw test

--- src/tests/entities/AccountTests.java
+++ src/tests/entities/AccountTests.java
@@ -11,9 +11,10 @@ public class AccountTests {
 	@Test
 	public void depositShouldIncreaseBalanceWhenPositiveAmount() {
 		
+		double initialBalance = 0.0;
 		double amount = 200.00;
 		double expectedValue = 196.0;
-		Account acc = AccountFactory.createEmptyAccount();
+		Account acc = AccountFactory.createAccount(initialBalance);
 		
 		acc.deposit(amount);
 		
@@ -24,13 +25,27 @@ public class AccountTests {
 	public void depositShouldDoNothingWhenNegativeAmount() {
 		
 		double expectedValue = 100.0;
-		Account acc = AccountFactory.createEmptyAccount(expectedValue);
+		Account acc = AccountFactory.createAccount(expectedValue);
 		double amount = - 200.0;
 		
 		acc.deposit(amount);
 		
 		Assertions.assertEquals(expectedValue, acc.getBalance());
 	}
+	
+	@Test
+	public void fullWithdrawShouldClearBalance() {
+		
+		double expectedValue = 0.0;
+		double initialBalance = 800.0;
+		Account acc = AccountFactory.createAccount(initialBalance);
+		
+		double result = acc.fullWithdraw();
+		
+		Assertions.assertTrue(expectedValue == acc.getBalance());
+		Assertions.assertTrue(result == initialBalance);
+	}
+	
 	
 	
 }
--- src/tests/factory/AccountFactory.java
+++ src/tests/factory/AccountFactory.java
@@ -3,12 +3,8 @@ package tests.factory;
 import entities.Account;
 
 public class AccountFactory {
-
-	public static Account createEmptyAccount() {
-		return new Account(1L, 0.0);
-	}
 	
-	public static Account createEmptyAccount(double initialBalance) {
+	public static Account createAccount(double initialBalance) {
 		return new Account(1L, initialBalance);
 	}
 	
